0dadd5a0d5b750d47bb669d82fce282bee88bcd8
Vivek R. Patil <vivekp@kesari.in>
2025-04-08 17:39:41 +0530

    minor changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { NewComComponent } from './new-com/new-com.component';
+
 
 export const routes: Routes = [
-    {path: 'newCom', component:NewComComponent},
+   
 ];
